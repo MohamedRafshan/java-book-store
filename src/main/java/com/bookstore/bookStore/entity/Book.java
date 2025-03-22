23813ac40b31e746d7d4d0202462d3bc8aed07fb
Rafshan Rakeeb <rakeebrafshan4@gmail.com>
2025-03-22 18:05:16 +0530

    book clsaa

--- src/main/java/com/bookstore/bookStore/entity/Book.java
+++ src/main/java/com/bookstore/bookStore/entity/Book.java
@@ -1,5 +1,21 @@
 package com.bookstore.bookStore.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String price;
+
+    
+
 
 }
